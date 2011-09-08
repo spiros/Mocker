293f2a6a93c4a68fd6ef69554fe5adb652529eed
Spiros Denaxas <s.denaxas@gmail.com>
2011-09-08 12:55:53 +0100

    basic writer class, prints to STDERR for now, implemented configuration reading/writing

---
+++ lib/Mocker/Writer.pm
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+package Mocker::Writer;
+# ABSTRACT: Data writer class.
+
+use Text::CSV;
+
+sub new {
+    my $class = shift;
+    my $self  = { };
+    
+    $self->{csv} = Text::CSV->new();
+    bless $self, $class;
+    return $self;
+    
+}
+
+sub write_row {
+    my $self    = shift;
+    my $ra_row  = shift;
+    
+    my $rv = $self->{csv}->combine( @$ra_row );
+    
+    if ( $rv ) {
+#        printf STDERR ("%s\n", $self->{csv}->string );
+    }
+    
+    else {
+        return undef;
+    }
+}
+
+1;
