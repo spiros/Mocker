bc5097f098eaccf61ff75ae11f1b7e1618f6d19c
Spiros Denaxas <s.denaxas@gmail.com>
2011-09-27 23:54:54 +0100

    one more example, this time writing data ta a file

---
+++ examples/ex2.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Mocker;
+
+my $rh_configuration = { 
+    
+    'variables' => {
+        
+        'first_name_male' => {
+            'order' => 0
+        },
+        
+        'number' => {
+            'order' => 1,
+            'min' => 1,
+            'max' => 5000,
+        }
+        
+    },
+    
+    'rows' => 500,
+    
+    'output' => {
+        'target'   => 'file',
+        'filename' => 'testdata.csv'
+    }
+    
+};
+
+my $Mocker = Mocker->new( $rh_configuration );
+
+$Mocker->generate;
