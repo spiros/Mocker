6d52cfbe424a1548ececb61afd70dfd611fb265e
Spiros Denaxas <s.denaxas@gmail.com>
2011-09-29 12:02:52 +0100

    some more POD documentation

--- lib/Mocker.pm
+++ lib/Mocker.pm
@@ -9,6 +9,105 @@ use Carp;
 use Data::Dumper;
 use Mocker::Writer;
 
+=head2 new
+
+Create new instance of the Mocker class. The new() method takes one parameter, a
+reference to hash with the configuration for the desired data. 
+
+    my $rh_configuration = { 
+    
+        'variables' => {
+        
+            'gender' => {
+                'order'    => 0,
+                'nicename' => 'sex'
+            },
+        
+            'surname' => {
+                'order' => 1
+            },
+        
+            'weight' => {
+                'order' => 2
+            },
+        
+            'number' => {
+                'order' => 3,
+                'min'   => 10,
+                'max'   => 20,
+                'round' => 1
+            }
+        
+        },
+    
+        'rows' => 100,
+    
+        'output' => {
+            'target'   => 'screen',
+        }
+    
+    };
+
+
+    my $Mocker = Mocker->new( $rh_configuration );
+
+    $Mocker->generate;
+
+=cut
+
+=head2 Configuration
+
+The following pre-defined canonical variable types are available:
+
+L<Mocker::Data::Medical::Gender>
+L<Mocker::Data::Medical::HeartRate>
+L<Mocker::Data::Medical::Height>
+L<Mocker::Data::Medical::Weight>
+L<Mocker::Data::Number>
+L<Mocker::Data::Person::Surname>
+L<Mocker::Data::Person::FirstNameFemale>
+L<Mocker::Data::Person::FirstNameMale>
+
+The 'variables' key of the configuration hash contains the canonical column names 
+along with some other per-variable configuration parameters. The I<order> contains the
+column position which you want to the variable to be placed in the output. By default,
+the variables canonical names will be used, this can be overwritten by using I<nicename>
+
+  'gender' => {
+        'order'    => 0,
+        'nicename' => 'sex'
+    },
+
+For numerical variables you can additionally specify a minimum I<min>, a maximum I<max> 
+and if you want to round the value.
+
+    'number' => {
+        'order' => 3,
+        'min'   => 10,
+        'max'   => 20,
+        'round' => 1
+    }
+
+You can specify the number of rows you want using the 'rows' key.
+
+    'rows' => 100,
+
+The output information is specified in the 'output' key. To write to STDOUT set the I<target>
+to I<screen> and to write to a file set the I<target> to I<file> and specify a filename using 
+I<filename>.
+
+    'output' => {
+        'target'   => 'screen',
+    }
+
+    'output' => {
+        'target'   => 'file',
+        'filename' => 'testdata.csv'
+    }
+
+Existing files will not be overwritten and will cause a fatal error.
+
+=cut
 sub new {
     my $class     = shift;
     my $rh_config = shift;
@@ -27,7 +126,6 @@ sub new {
     
     return $self;
 }
-
 
 sub parse_configuration {
     my $self = shift;
@@ -61,6 +159,14 @@ sub parse_configuration {
     
     return $rh_config;
 }
+
+=head2 generate
+
+Generate the data and write to supplied output method.
+
+    $Mocker->generate;
+
+=cut
 
 sub generate {
     my $self = shift;
